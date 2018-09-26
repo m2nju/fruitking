1e937700356f981551b95d92fed72abe10dc58ed
leeminju <leeminju@DESKTOP-93OGG00>
2018-09-26 22:00:02 +0900

    .

--- fruitking/src/main/java/kr/ac/hongik/fruitking/config/DBConfig.java
+++ fruitking/src/main/java/kr/ac/hongik/fruitking/config/DBConfig.java
@@ -28,6 +28,38 @@ public class DBConfig implements TransactionManagementConfigurer {
 		dataSource.setUrl(url);
 		dataSource.setUsername(username);
 		dataSource.setPassword(password);
+		
+		// 초기에 생성되는 커넥션의 개수 설정
+		dataSource.setInitialSize(4);
+		
+		// 유휴 커넥션 검사 주기, 사용되지 않은 커넥션을 추출하는 쓰레드의 실행주기를 지정한다.
+		dataSource.setTimeBetweenEvictionRunsMillis(1000L * 60L * 1L);
+		
+		// 풀에 있는 커넥션이 유효한지 검사 유무 설정, true 일 경우 비활성화 커넥션을 추출할 때 커넥션이 유효한지의 여부를 검사해서 유효하지 않은 커넥션은 풀에서 제거
+		dataSource.setTestWhileIdle(true);
+		
+		// 커넥션 최소 개수 설정, 사용되지 않고 풀에 저장될 수 있는 최소 커넥션 개수
+		dataSource.setMinIdle(4);
+		
+		// 커넥션 최대 개수 설정, 사용되지 않고 풀에 저장될 수 있는 최대 커넥션 개수
+		dataSource.setMaxIdle(20);
+		
+		// true 일 경우 커넥션 풀에서 커넥션을 가져올 때 커넥션이 유효한지의 여부 검사
+		dataSource.setTestOnBorrow(false);
+		
+		// true 일 경우 비활성화 커넥션을 추출할 때 커넥션이 유효한지의 여부를 검사해서 유효하지 않은 커넥션은 풀에서 제거
+		dataSource.setTestWhileIdle(true);
+		
+		// 사용되지 않는 커넥션을 몇 개 검사할 지 지정
+		dataSource.setNumTestsPerEvictionRun(3);
+		
+		// PreparedStatements를 풀링 할 것인지 결정한다.
+		// PreparedStatement란 이처럼 바인드 변수의 사용을 가능케 하기 위해 사용됩니다. 
+		// 따라서 동일한 문장(위처럼 값만 바뀐다거나 할때)이 여러 번 연속으로 실행되거나 비교적 연속적(라이브러리 캐쉬에 있는
+		// 실행계획이 LRU에 의해 메모리에서 삭제되기 직전에 다시 실행되기만 한다면)으로 실행된다면 월등한 성능향상을 얻을 수 있습니다.
+		// 그러므로 꼭 사용하도록 설정한다.( true로 설정해준다.)
+		dataSource.setPoolPreparedStatements(true);
+		
 		return dataSource;
 	}
 
